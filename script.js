245cf16b6133eba5f815bae2be3c8e2228e1ab5d
TrojanYT <roroge.superpositivo@gmail.com>
2023-07-19 23:05:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 function animateStateOrder() {
-    const container = document.querySelector('.container');
-    const states = Array.from(container.querySelectorAll('.state'));
-  
-    states.sort((a, b) => {
-      const scoreA = parseInt(a.dataset.score);
-      const scoreB = parseInt(b.dataset.score);
-      return scoreB - scoreA; // Ordena em ordem decrescente com base nos pontos
-    });
-  
-    states.forEach((state, index) => {
-      state.style.transform = `translateY(${index * 70}px)`; // Ajuste o valor '70' para a altura do estado
-    });
-  }
-  
-  setInterval(animateStateOrder, 5000); // Atualiza a animação a cada 2 segundos (ajuste o valor conforme necessário)
-  
+  const container = document.querySelector('.container');
+  const states = Array.from(container.querySelectorAll('.state'));
+
+  // Redefine as transformações de todos os estados
+  states.forEach((state) => {
+    state.style.transform = '';
+  });
+
+  // Ordena os estados com base nos pontos
+  states.sort((a, b) => {
+    const scoreA = parseInt(a.dataset.score);
+    const scoreB = parseInt(b.dataset.score);
+    return scoreB - scoreA; // Ordena em ordem decrescente com base nos pontos
+  });
+
+  let offsetY = 0;
+  states.forEach((state, index) => {
+    const stateHeight = state.offsetHeight + 5; // Ajuste o valor '5' conforme necessário para o espaçamento entre estados
+    state.style.transform = `translateY(${offsetY}px)`;
+    offsetY += stateHeight;
+  });
+}
+
+animateStateOrder(); // Chama a função uma vez para fazer a animação inicial
+setInterval(animateStateOrder, 2000); // Atualiza a animação a cada 2 segundos (ajuste o valor conforme necessário)
